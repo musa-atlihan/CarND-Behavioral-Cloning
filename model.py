80cb8e27afed05a9c913736862562a25960eaac7
wphw <wphwoe@gmail.com>
2017-08-02 14:50:09 +0300

    add exclusion function to the driving data for zero angle values

--- model.py
+++ model.py
@@ -4,21 +4,23 @@
 # Coded by: Musa Atlıhan (github.com/wphw)
 # 2017-07-29 (github.com/wphw/CarND-Behavioral-Cloning-P3)
 
+from utils import *
 
 # parameters
-csv_path = 'data/driving_log.csv'
+csv_path = 'data_xl/'
+csv_name = 'driving_log.csv'
 save_name = 'weights-best.hdf5'
 batch_size = 32
-epochs = 10
+epochs = 5
 
 
 # load samples
 import csv
 
 samples = []
-with open(csv_path) as f:
+with open(csv_path + csv_name) as f:
     reader = csv.reader(f)
-    next(reader)
+    #next(reader)
     for row in reader:
         samples.append(row)
 
@@ -27,27 +29,51 @@ with open(csv_path) as f:
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
-train_samples, valid_samples = train_test_split(samples, test_size=0.3, random_state=2)
+train_samples, valid_samples = train_test_split(samples, test_size=0.3, random_state=100)
 
 
 import cv2
 import numpy as np
 
+train_samples = np.array(train_samples)
+print(train_samples.shape)
+# reduce the number of rows with angle=0
+train_samples = exclude_by_value(train_samples, 3, '0', pct=85)
+print(train_samples.shape)
+valid_samples = np.array(valid_samples)
+
 # generator for to load the batch
 def generator(samples, batch_size=32):
-    n_samples = len(samples)
+    n_samples = samples.shape[0]
     while 1:
         for offset in range(0, n_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
             images = []
             angles = []
+            correction = 0.2
             for batch_sample in batch_samples:
-                path = './data/IMG/' + batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(path)
+                left_path = './' + csv_path + 'IMG/' + batch_sample[1].split('/')[-1]
+                center_path = './' + csv_path + 'IMG/' + batch_sample[0].split('/')[-1]
+                right_path = './' + csv_path + 'IMG/' + batch_sample[2].split('/')[-1]
+
+                left_image = cv2.imread(left_path)
+                center_image = cv2.imread(center_path)
+                right_image = cv2.imread(right_path)
+
+                #flipped_left_image = cv2.flip(left_image, 1)
+                #flipped_center_image = cv2.flip(center_image, 1)
+                #flipped_right_image = cv2.flip(right_image, 1)
+                
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                left_angle = center_angle + correction
+                right_angle = center_angle - correction
+                
+                #images.extend([left_image, center_image, right_image, 
+                #                flipped_left_image, flipped_center_image, flipped_right_image])
+                images.extend([left_image, center_image, right_image])
+                #angles.extend([left_angle, center_angle, right_angle,
+                #                -left_angle, -center_angle, -right_angle])
+                angles.extend([left_angle, center_angle, right_angle])
             X = np.array(images)
             y = np.array(angles)
             yield shuffle(X, y)
@@ -68,8 +94,8 @@ from keras.callbacks import ModelCheckpoint
 # the model
 model = Sequential()
 
-model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
-model.add(Lambda(lambda x: x / 127.5 - 1.))
+model.add(Cropping2D(cropping=((60, 20), (0, 0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: (x / 127.5) - 1.))
 model.add(Conv2D(24, (5, 5), strides=(2, 2), activation='relu'))
 model.add(Conv2D(36, (5, 5), strides=(2, 2), activation='relu'))
 model.add(Conv2D(48, (5, 5), strides=(2, 2), activation='relu'))
@@ -84,16 +110,15 @@ model.add(Dense(10, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(1))
 
+#sgd = keras.optimizers.SGD(lr=0.01)
 model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 
 # callbacks
-checkpoint = ModelCheckpoint(save_name, monitor='val_acc', verbose=1,
-                                                    save_best_only=True, mode='max')
+checkpoint = ModelCheckpoint(save_name, monitor='val_loss', verbose=1,
+                                                    save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 
-model.fit_generator(train_generator, steps_per_epoch=len(train_samples)//batch_size,
+model.fit_generator(train_generator, steps_per_epoch=(len(train_samples) * 3)//batch_size,
                     validation_data=valid_generator, validation_steps=\
-                    len(valid_samples)//batch_size, callbacks=callbacks_list,
+                    (len(valid_samples) * 3)//batch_size, callbacks=callbacks_list,
                     epochs=epochs)
-
-model.save(save_name)
---
+++ utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def exclude_by_value(array, col, val, pct=100):
+    """Remove numpy array elements that are equals to a 
+    specific value.
+    
+    Args:
+        array: numpy array to be elements removed.
+        col: integer represents the indis of the column 
+             that will be searched.
+        val: value in the column to be removed.
+        pct: percentage of removed elements. 
+             example: (integer)100 removes all.
+    """
+
+    rows = np.where(array[:,col] == val)[0]
+    rows_not = np.where(array[:,col] != val)[0]
+    perm = np.random.permutation(rows.shape[0])
+    pct_left = int(rows.shape[0] * (100 - pct) / 100)
+    shuff_rows = rows[perm][0:pct_left]
+    index = np.concatenate((rows_not, shuff_rows))
+
+    return array[index]
+
